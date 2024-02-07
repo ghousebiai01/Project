f2e0c68898b2773872efbffcc2d9c94871ad64c2
Ghousebi <ghousebiai01@gmail.com>
2024-02-07 13:39:09 +0530

    added

---
+++ Python/Pythondemo/functionsdemo.py
@@ -0,0 +1,24 @@
+
+"""def myaddition():
+    a=6
+    print(a)
+    if a==3:
+        print("a is 3")
+        x= "This is addition demo"
+        for x1 in x:
+            print(x1)
+        x=x.split(" ")
+        for x2 in x:
+            print (x2)
+    elif  a==6:
+          print("a is greater than 6")
+    y=input( ).split()
+    print(y)
+myaddition()
+r="Ghouse bi"
+print(r[1:4])
+print(r[:5])"""
+
+age = "thirty four"
+txt = "My name is John, I am " + age
+print(txt)
--- Python/Pythondemo/w3schoolspractice.py
+++ Python/Pythondemo/w3schoolspractice.py
@@ -1,4 +1,14 @@
-a="beautiful"
-def mycond():
- print("life is "+a)
-mycond()
+
+import random
+print (random.randrange(10,90))
+print('hello')
+a= """Hi guys! How are you? Is this of for you? what about youself?"""
+for x in a:
+    print(x)
+a= a.split(" ")
+print (a)
+for n in a:
+    print(n)
+a2="Happy Life"
+if "Happy" in a2:
+    print("Yes you are right")
